6358950ab77452c7cc390ff9323152e43ec91e9c
Doronico324 <doronico324@gmail.com>
2023-12-14 11:11:40 +0200

    cors fix

--- Store_Proj/settings.py
+++ Store_Proj/settings.py
@@ -61,6 +61,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "https://doronico324.github.io",
+    "http://localhost:3000/react-store",
 ]
 
 MIDDLEWARE = [
